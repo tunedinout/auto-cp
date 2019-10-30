624c2eb279b3cc934db79c8215dab52523135434
tunedinout <narottam2911@gmail.com>
2019-10-30 19:41:58 +0530

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//console.log('build started');
+
+module.exports = {
+  mode: 'production',
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    //publicPath: '/build/',
+    filename: 'index.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        //include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-transform-react-jsx"
+            ]
+          }
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|jpeg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'logo.jpeg',
+              outputPath: '/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+};
